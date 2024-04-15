828ca1b0deec35d221a4751bdec5e6dbf62eece5
Jiya <coderaholic21@gmail.com>
2024-04-15 20:56:46 +0530

    Added Register component

---
+++ src/components/Register.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <form class="r-form">
+    <input class="r-input" type="text" placeholder="Name"/>
+    <input class="r-input" type="text" placeholder="E-Mail I.D."/>
+    <textarea class="r-textarea" placeholder="Enter message"></textarea> 
+    <center><button class="r-button">Submit</button></center>
+</form>
+    </div>
+  )
+}
+
+export default Register

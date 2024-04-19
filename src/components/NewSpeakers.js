86f5eaaf69a3458be150fc2b2f5dc6c7432352b8
Jiya <coderaholic21@gmail.com>
2024-04-19 19:59:46 +0530

    Added links to speaker cards

--- src/components/NewSpeakers.js
+++ src/components/NewSpeakers.js
@@ -15,12 +15,19 @@ const NewSpeakers = () => {
                     <p class="heading">
                         Akshay Saini
                     </p>
-                    <p>Associate Sponsor
+                    <p>Namaste Dev, Founder
                     </p>
                     <div className="icon flex gap-5">
+                        <a href="https://www.linkedin.com/in/akshaymarch7/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
                         <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
+                        </a>                        
+
+                        <a href="">
+                        <img width={'30px'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_fH0Xv1smcTf_BF9X62XnrDo7ZpDXJJvN87bjv_st3A&s" alt="" />
+                        </a>
+
+                        <a href="https://www.instagram.com/akshaymarch7/?igsh=MTl2bTEwZmx6b3oxbg%3D%3D">
+                        <img width={'30px'} src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" alt="" /></a>
                     </div>
                     </div>
                     <div class="xcard">
@@ -29,12 +36,19 @@ const NewSpeakers = () => {
                     <p class="heading">
                     Keerti Purswani
                     </p>
-                    <p>Associate Sponsor
+                    <p>Educosys , Founder
                     </p>
                     <div className="icon flex gap-5">
+                    <a href="https://www.linkedin.com/in/keertipurswani?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
                         <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
+                        </a>                        
+
+                        <a href="">
+                        <img width={'30px'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_fH0Xv1smcTf_BF9X62XnrDo7ZpDXJJvN87bjv_st3A&s" alt="" />
+                        </a>
+
+                        <a href="https://www.instagram.com/keerti.purswani/?igsh=bGwwZmU3OTQ5cXJx">
+                        <img width={'30px'} src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" alt="" /></a>
                     </div>
                     </div>
                     <div class="xcard">
@@ -43,12 +57,19 @@ const NewSpeakers = () => {
                     <p class="heading">
                     Supriya Purohit
                     </p>
-                    <p>Associate Sponsor
+                    <p>Product manager, ex- Flipkart
                     </p>
                     <div className="icon flex gap-5">
+                    <a href="https://www.linkedin.com/in/supriyapurohit27/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
                         <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
-                        <img width={'30px'} src="https://static-00.iconduck.com/assets.00/linkedin-icon-256x256-i5hp0ntv.png" alt="" />
+                        </a>                        
+
+                        <a href="">
+                        <img width={'30px'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_fH0Xv1smcTf_BF9X62XnrDo7ZpDXJJvN87bjv_st3A&s" alt="" />
+                        </a>
+
+                        <a href="https://www.instagram.com/supriyapurohittt/?igsh=eWRqNWd1Z3dpaTg3">
+                        <img width={'30px'} src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" alt="" /></a>
                     </div>
                     </div>
                 

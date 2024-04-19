c5898d6614ca21d4639637a3877e5426a96ce38c
Jiya <coderaholic21@gmail.com>
2024-04-19 18:47:43 +0530

    speaker cards

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ import Sponsors from "./components/Sponsors";
 import Footer from "./components/Footer";
 import NewNavbar from "./components/NewNavbar";
 import NewTimeline from "./components/NewTimeline";
+import NewSpeakers from "./components/NewSpeakers";
 
 const Home = () => {
   return (
@@ -25,7 +26,8 @@ const Home = () => {
         ></div>
       </Fragment>
       <HomeBanner />
-      <Timeline />
+      {/* <Timeline /> */}
+      <NewSpeakers/>
       <NewTimeline/>
       <Sponsors />
       <FAQ />
